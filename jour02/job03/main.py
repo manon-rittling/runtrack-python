a50fc3dcd2f657f1c3ffeac28645356280329926
manon-rittling <manon.rittling@laplateforme.io>
2023-11-21 10:40:54 +0100

    job03

---
+++ jour02/job03/main.py
@@ -0,0 +1,10 @@
+for i in range (101):
+    if i not in [26,37,88]:  
+        print (i)
+
+
+# proposition 2 
+
+#  for i in range (101):
+    # if i !=26 and i !=37 and i !=88 :
+            # print (i)

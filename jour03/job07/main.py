0db9bdca37106a6806dbfb04deb5ec1d08c480fc
manon-rittling <manon.rittling@laplateforme.io>
2023-11-22 11:49:11 +0100

    job07

---
+++ jour03/job07/main.py
@@ -0,0 +1,16 @@
+def determineDomaine(langage):
+    if langage == "javaScript":
+        print("tu es un développeur web")
+    elif langage == "python":
+        print("tu es un développeur IA")
+    elif langage == "java":
+        print("tu es un développeur logiciel")
+    elif langage == "reactjs":
+        print("tu es un developpeur mobile")
+    else:
+        print("un jour, je serai le meilleur développeur...")
+    
+determineDomaine("javaScript")
+determineDomaine("python")
+determineDomaine("reactjs")
+determineDomaine("c++")

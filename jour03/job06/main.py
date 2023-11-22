d31b561aba17a396873c6bf453f0bd7852536680
manon-rittling <manon.rittling@laplateforme.io>
2023-11-22 11:30:18 +0100

    job06

---
+++ jour03/job06/main.py
@@ -0,0 +1,11 @@
+def affichageNombre(nombre):
+    if nombre > 0 :
+        print("positif")
+    elif nombre < 0 :
+        print("negatif")
+    else:
+        print("le nombre est égal a 0")
+
+affichageNombre(7)
+affichageNombre(-3)
+affichageNombre(0)

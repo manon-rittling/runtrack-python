fd2009f8f096b67027e58797b3767d29f7c08da2
manon-rittling <manon.rittling@laplateforme.io>
2023-11-23 21:15:19 +0100

    job12 fini

--- jour04/job12/main.py
+++ jour04/job12/main.py
@@ -13,12 +13,13 @@ def longueur(liste):
 def trier(liste):
     echange = True #echange effectué
     while echange:
-        echange = False #on passe l'echange en faux 
+        echange = False # On initialise l'échange à False avant chaque nouvelle itération 
         index = 1 #commence a l'index 1 pour qu'il puisse comparé avec l'index0
         while index < longueur(liste):
+            # comparaison et échange si chiffre n'est pas en ordre croissant 
             if liste[index-1] > liste[index]:
                 liste[index-1], liste[index] = liste[index], liste[index-1]
-                echange = True
+                echange = True #si un échange est fait, on le repasse en True
             index += 1
     return liste
 

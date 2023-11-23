d92d8ff142e7daa25720b53b73293cdaf168e95f
manon-rittling <manon.rittling@laplateforme.io>
2023-11-23 12:32:21 +0100

    job09

---
+++ jour04/job09/main.py
@@ -0,0 +1,8 @@
+def recup():
+    L = [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91]
+    valeurMax = max(L)
+    valeurMin = min(L)
+    return (valeurMax, valeurMin)
+
+print(f'affiche la valeur max et minimum', recup())
+
